7f2012e3ffb22863cb813e0a7353c3c75d816dae
Dima <39806138+morozdima@users.noreply.github.com>
2020-08-11 11:20:02 +0300

    Update current_orders.py

--- sourse/ui/modules/current_orders.py
+++ sourse/ui/modules/current_orders.py
@@ -14,10 +14,12 @@ class CurrentOrdersModule(BaseUIModule):
         self._current_id = 0
         self._order_dict = {}
 
-        self.table = QtWidgets.QTableWidget(len(self._order_dict), 5)
+        self.table = QtWidgets.QTableWidget(len(self._order_dict), 6)
 
-        self.table.setSortingEnabled(False)
-        self.table.setHorizontalHeaderLabels(["id", "Time", "Side", "Price", "Volume"])
+        self.table.setSortingEnabled(True)
+        self.table.setHorizontalHeaderLabels(
+            ["id", "Time", "Side", "Price", "Volume", "Status"]
+        )
         self.table.verticalHeader().hide()
 
         header = self.table.horizontalHeader()
@@ -26,10 +28,18 @@ class CurrentOrdersModule(BaseUIModule):
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(5, QtWidgets.QHeaderView.Stretch)
 
         self.layout.addWidget(self.table)
 
-    def add_order(self, order_id: str, price: float, volume: float) -> int:
+    def add_order(self, order_id: str, price: float, volume: float, status: str) -> str:
+        if order_id in self._order_dict.keys():
+            self._edit_order(
+                order_id=order_id, price=price, volume=volume, status=status
+            )
+
+        self.table.setSortingEnabled(False)
+
         self._current_id += 1
         order_id = self._current_id
 
@@ -43,28 +53,70 @@ class CurrentOrdersModule(BaseUIModule):
                 len(self._order_dict) - 1, i + 1, self.createItem(str(value))
             )
 
+        self.table.setSortingEnabled(True)
+
+        return order_id
+
+    def _edit_order(
+        self, order_id: str, price: float, volume: float, status: str
+    ) -> str:
+        self.table.setSortingEnabled(False)
+
+        tmp_order = self._order_dict[order_id]
+        order_index = list(self._order_dict.keys()).index(order_id)
+
+        if price != tmp_order.price:
+            self._order_dict[order_id].price = price
+            self.table.item(3, order_index).setText(str(price))
+        if volume != tmp_order.volume:
+            self._order_dict[order_id].volume = volume
+            self.table.item(4, order_index).setText(str(volume))
+        if status != tmp_order.status:
+            self._order_dict[order_id].status = status
+            self.table.item(5, order_index).setText(str(status))
+
+        self.table.setSortingEnabled(True)
+
         return order_id
 
     def remove_order(self, order_id: int) -> None:
-        del self._order_dict[order_id]
-        self.table.removeRow(order_id - 1)
+        self.table.setSortingEnabled(False)
 
-    def createItem(self, text):
+        del self._order_dict[order_id]
+        self.table.removeRow(list(self._order_dict.keys()).index(order_id))
+
+        self.table.setSortingEnabled(True)
+
+    def remove_all_orders(self) -> None:
+        self.table.setSortingEnabled(False)
+
+        self._order_dict = {}
+        self.table.setRowCount(0)
+
+        self.table.setSortingEnabled(True)
+
+    @staticmethod
+    def createItem(text: str) -> QtWidgets.QTableWidgetItem:
         tableWidgetItem = QtWidgets.QTableWidgetItem(text)
         tableWidgetItem.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
         return tableWidgetItem
 
 
 class Order:
-    def __init__(self, price: float, volume: float):
+    def __init__(self, price: float, volume: float, status: str = None):
         self.price = price
         self.volume = volume
 
         self.side = "Buy" if volume > 0 else "Sell"
-        self.date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.status = status
 
     def to_dict(self):
         return dict(
-            time=self.date, side=self.side, price=self.price, volume=abs(self.volume)
+            time=self.time,
+            side=self.side,
+            price=self.price,
+            volume=abs(self.volume),
+            status=self.status,
         )
-
